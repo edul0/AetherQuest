8ebaeccfb959ca5ea53bc690abc6ede351527e03
EduardoL <eduardolesse0@gmail.com>
2026-04-17 08:16:46 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+// Colocando as chaves diretamente para a Vercel não quebrar o build
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://bekjfcjlywgrsfqkakfn.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || 'sb_publishable_GdYDi1FKgtsdbb3HIHxpIA_Jm2y1HFA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
