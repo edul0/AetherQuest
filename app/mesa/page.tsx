45d6ad4fdb286063384043c449ea25231e890c97
EduardoL <eduardolesse0@gmail.com>
2026-04-20 12:09:16 -0300

    Stabilize mesa route and token creation flow

--- app/mesa/page.tsx
+++ app/mesa/page.tsx
@@ -1,10 +1,5 @@
 import MesaClient from "@/src/components/vtt/MesaClient";
-import RequireAuth from "@/src/components/auth/RequireAuth";
 
 export default function MesaPage() {
-  return (
-    <RequireAuth>
-      <MesaClient />
-    </RequireAuth>
-  );
+  return <MesaClient />;
 }
