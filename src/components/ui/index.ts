12fbe91d47fbf6b18dd1854d4ec94db7c717306b
EduardoL <eduardolesse0@gmail.com>
2026-04-28 08:35:09 -0300

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,14 @@
+// Layout Components
+export { PageLayout, PageShell, PageHeader, PageContent, Grid, Flex } from "./Layout";
+
+// UI Components
+export { Card, CardHeader, CardContent, CardFooter } from "./Card";
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Badge } from "./Badge";
+export { Modal, useModal } from "./Modal";
+export { Alert } from "./Alert";
+export { Tabs } from "./Tabs";
+
+// Typography
+export { H1, H2, H3, H4, Body, Subtitle, Caption } from "./Typography";
