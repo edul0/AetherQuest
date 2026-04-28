ed25ec151df4477f9b807103d27320770f912cd4
EduardoL <eduardolesse0@gmail.com>
2026-04-28 14:40:38 -0300

    Refine UI foundation exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,14 +1,14 @@
-// Layout Components
 export { PageLayout, PageShell, PageHeader, PageContent, Grid, Flex } from "./Layout";
 
-// UI Components
 export { Card, CardHeader, CardContent, CardFooter } from "./Card";
 export { Button } from "./Button";
 export { Input } from "./Input";
 export { Badge } from "./Badge";
-export { Modal, useModal } from "./Modal";
 export { Alert } from "./Alert";
-export { Tabs } from "./Tabs";
+export { Select } from "./Select";
+export type { SelectOption } from "./Select";
+export { StatCard, StatGrid } from "./StatCard";
+export { Separator } from "./Separator";
+export { FormGroup } from "./FormGroup";
 
-// Typography
 export { H1, H2, H3, H4, Body, Subtitle, Caption } from "./Typography";
